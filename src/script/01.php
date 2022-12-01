34ecb5def2b18e49a5f8d177437e3f7ecca7f570
Juan Garcia <jcruzgar@gmail.com>
2022-12-01 19:40:40 +0100

    feat(Day 01): day 01 input provided and solved
    - bug fixed on resseting the counter

--- src/script/01.php
+++ src/script/01.php
@@ -5,20 +5,19 @@ use Jdlcgarcia\Aoc2022\common\FileHandler;
 require_once 'vendor/autoload.php';
 
 $fileHandler = new FileHandler();
-$file = $fileHandler->loadFileContent('test.txt');
+$file = $fileHandler->loadFileContent('01.txt');
 
 $max = 0;
 $caloriesCounter = 0;
 while (!$file->eof()) {
     if ($file->current() === PHP_EOL) {
-        if ($max < $caloriesCounter) {
+        if ($max <= $caloriesCounter) {
             $max = $caloriesCounter;
-            $caloriesCounter = 0;
         }
+        $caloriesCounter = 0;
     } else {
         $caloriesCounter += (int)$file->current();
     }
-
     $file->next();
 }
 
