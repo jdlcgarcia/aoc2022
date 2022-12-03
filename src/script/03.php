a5fcf5ad96c60dcc7cd857d5cfd291f6a73f2c59
Juan Garcia <jcruzgar@gmail.com>
2022-12-03 14:49:46 +0100

    feat(Day 03): executing script over the real input

--- src/script/03.php
+++ src/script/03.php
@@ -6,7 +6,7 @@ use Jdlcgarcia\Aoc2022\entities\Rucksack;
 require_once 'vendor/autoload.php';
 
 $fileHandler = new FileHandler();
-$file = $fileHandler->loadFileContent('test.txt');
+$file = $fileHandler->loadFileContent('03.txt');
 
 $priorities = 0;
 while (!$file->eof()) {
