d871e38c5d965fabdc93fe03f7cc829645b8adbd
Juan Garcia <jcruzgar@gmail.com>
2022-12-03 14:48:41 +0100

    feat(Day 03): script to calculate priorities

---
+++ src/script/03.php
@@ -0,0 +1,22 @@
+<?php
+
+use Jdlcgarcia\Aoc2022\common\FileHandler;
+use Jdlcgarcia\Aoc2022\entities\Rucksack;
+
+require_once 'vendor/autoload.php';
+
+$fileHandler = new FileHandler();
+$file = $fileHandler->loadFileContent('test.txt');
+
+$priorities = 0;
+while (!$file->eof()) {
+    if (trim($file->current()) !== '') {
+        $rucksack = new Rucksack();
+        $rucksack->fill(trim($file->current()));
+        $priorities += $rucksack->getRepeatedPriority();
+    }
+
+    $file->next();
+}
+
+echo $priorities . PHP_EOL;
