af177f9717f0fc7f78a677524f9d4e1c7a5a5afa
Juan Garcia <jcruzgar@gmail.com>
2022-12-04 09:08:29 +0100

    feat(Day 04): refactored name of function

--- src/script/04.php
+++ src/script/04.php
@@ -6,18 +6,18 @@ use Jdlcgarcia\Aoc2022\entities\CleanupAssignment;
 require_once 'vendor/autoload.php';
 
 $fileHandler = new FileHandler();
-$file = $fileHandler->loadFileContent('04.txt');
-$countOverlaps = 0;
+$file = $fileHandler->loadFileContent('test.txt');
+$countCompleteOverlaps = 0;
 while (!$file->eof()) {
     if (trim($file->current()) !== '') {
         [$firstStart, $firstEnd, $secondStart, $secondEnd] = sscanf(trim($file->current()), '%d-%d,%d-%d');
         $cleanupAssignment = new CleanupAssignment($firstStart, $firstEnd, $secondStart, $secondEnd);
-        if ($cleanupAssignment->findOverlap()) {
-            $countOverlaps++;
+        if ($cleanupAssignment->findCompleteOverlap()) {
+            $countCompleteOverlaps++;
         }
     }
 
     $file->next();
 }
 
-echo $countOverlaps . PHP_EOL;
+echo $countCompleteOverlaps . PHP_EOL;
