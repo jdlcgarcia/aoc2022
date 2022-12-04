f830eaa522f22ec7c67d635902bf72c5b91ff100
Juan Garcia <jcruzgar@gmail.com>
2022-12-04 08:54:02 +0100

    feat(Day 04): created script to count assignments

---
+++ src/script/04.php
@@ -0,0 +1,23 @@
+<?php
+
+use Jdlcgarcia\Aoc2022\common\FileHandler;
+use Jdlcgarcia\Aoc2022\entities\CleanupAssignment;
+
+require_once 'vendor/autoload.php';
+
+$fileHandler = new FileHandler();
+$file = $fileHandler->loadFileContent('test.txt');
+$countOverlaps = 0;
+while (!$file->eof()) {
+    if (trim($file->current()) !== '') {
+        [$firstStart, $firstEnd, $secondStart, $secondEnd] = sscanf(trim($file->current()), '%d-%d,%d-%d');
+        $cleanupAssignment = new CleanupAssignment($firstStart, $firstEnd, $secondStart, $secondEnd);
+        if ($cleanupAssignment->findOverlap()) {
+            $countOverlaps++;
+        }
+    }
+
+    $file->next();
+}
+
+echo $countOverlaps . PHP_EOL;
