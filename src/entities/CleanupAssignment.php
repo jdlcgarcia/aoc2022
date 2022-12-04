0f9f79eaa85de8c8ef404352be74dfbd469af13e
Juan Garcia <jcruzgar@gmail.com>
2022-12-04 09:15:22 +0100

    feat(Day 04): refactored to use arrays as it should have been before

--- src/entities/CleanupAssignment.php
+++ src/entities/CleanupAssignment.php
@@ -4,22 +4,27 @@ namespace Jdlcgarcia\Aoc2022\entities;
 
 class CleanupAssignment
 {
-    private string $firstAssignment = '';
-    private string $secondAssignment = '';
+    private array $firstAssignment = [];
+    private array $secondAssignment = [];
 
     public function __construct(int $firstStart, int $firstEnd, int $secondStart, int $secondEnd)
     {
         for ($i = $firstStart; $i <= $firstEnd; $i++) {
-            $this->firstAssignment .=  '#'. $i . '#';
+            $this->firstAssignment[] =  $i;
         }
         for ($i = $secondStart; $i <= $secondEnd; $i++) {
-            $this->secondAssignment .= '#'. $i . '#';
+            $this->secondAssignment[] = $i;
         }
     }
 
     public function findCompleteOverlap(): bool
     {
-        return !empty(str_contains($this->firstAssignment, $this->secondAssignment))
-            || !empty(str_contains($this->secondAssignment, $this->firstAssignment));
+        return empty(array_diff($this->firstAssignment, $this->secondAssignment))
+            || empty(array_diff($this->secondAssignment, $this->firstAssignment));
+    }
+
+    public function findPartialOverlap(): bool
+    {
+        return !empty(array_intersect($this->firstAssignment, $this->secondAssignment));
     }
 }
