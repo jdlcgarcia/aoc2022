8aad1cda3fd41d923d524a19620292a6791c83c5
Juan Garcia <jcruzgar@gmail.com>
2022-12-06 12:13:45 +0100

    feat(Day 06): logic to find the second part

--- src/entities/CommDevice.php
+++ src/entities/CommDevice.php
@@ -5,7 +5,8 @@ namespace Jdlcgarcia\Aoc2022\entities;
 class CommDevice
 {
     private string $dataStream;
-    private int $startOfPacketMarker = -1;
+    private int $startOfPacketMarker;
+    private int $startOfMessageMarker;
 
     public function __construct(string $dataStream)
     {
@@ -14,17 +15,16 @@ class CommDevice
 
     public function findStartOfPacketMarker(): int
     {
-        $positionToCheck = 0;
-        while($this->startOfPacketMarker === -1) {
-            $markerToCheck = str_split(substr($this->dataStream, $positionToCheck, 4));
-            if (sizeof(array_unique($markerToCheck)) === 4) {
-                $this->startOfPacketMarker = $positionToCheck + 4;
-            }
-
-            $positionToCheck++;
-        }
+        $this->startOfPacketMarker = $this->findMarkerBySize(4);
 
         return $this->startOfPacketMarker;
+    }
+
+    public function findStartOfMessageMarker(): int
+    {
+        $this->startOfMessageMarker = $this->findMarkerBySize(14);
+
+        return $this->startOfMessageMarker;
     }
 
     /**
@@ -41,5 +41,35 @@ class CommDevice
     public function getStartOfPacketMarker(): int
     {
         return $this->startOfPacketMarker;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStartOfMessageMarker(): int
+    {
+        return $this->startOfMessageMarker;
+    }
+
+    /**
+     * @param int $sizeOfMarker
+     * @return void
+     */
+    public function findMarkerBySize(int $sizeOfMarker): int
+    {
+        $positionToCheck = 0;
+        $marker = -1;
+
+        while ($marker === -1) {
+            $markerToCheck = str_split(substr($this->dataStream, $positionToCheck, $sizeOfMarker));
+            //var_dump($markerToCheck);
+            if (sizeof(array_unique($markerToCheck)) === $sizeOfMarker) {
+                $marker = $positionToCheck + $sizeOfMarker;
+            }
+
+            $positionToCheck++;
+        }
+
+        return $marker;
     }
 }
--- src/script/06.php
+++ src/script/06.php
@@ -6,12 +6,13 @@ use Jdlcgarcia\Aoc2022\common\FileHandler;
 use Jdlcgarcia\Aoc2022\entities\CommDevice;
 
 $fileHandler = new FileHandler();
-$file = $fileHandler->loadFileContent('test.txt');
+$file = $fileHandler->loadFileContent('06.txt');
 
 while (!$file->eof()) {
     if (trim($file->current()) !== '') {
         $commDevice = new CommDevice(trim($file->current()));
         echo $commDevice->findStartOfPacketMarker() . PHP_EOL;
+        echo $commDevice->findStartOfMessageMarker() . PHP_EOL;
     }
 
     $file->next();
