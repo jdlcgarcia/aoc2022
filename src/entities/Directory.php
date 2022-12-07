a18bf28dde7a75f20d19c69a3e76bee9954721e6
Juan Garcia <jcruzgar@gmail.com>
2022-12-07 12:20:52 +0100

    feat(Day 07): created movement within directories feature

--- src/entities/Directory.php
+++ src/entities/Directory.php
@@ -6,12 +6,18 @@ class Directory
 {
     private int $size;
     private string $name;
+    private ?Directory $parent = null;
 
     /** @var File[] $files */
     private array $files = [];
 
     /** @var Directory[] $subdirectories */
     private array $subdirectories = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
 
     /**
      * @return int
@@ -45,5 +51,32 @@ class Directory
         return $this->subdirectories;
     }
 
+    public function getDirectory(string $directory)
+    {
+        return $this->subdirectories[$directory];
+    }
 
+    public function getPath(): string
+    {
+        if (is_null($this->parent)) {
+            return '';
+        }
+
+        return $this->parent->getPath() . '/' . $this->getName();
+    }
+
+    public function setParent(Directory $parentDirectory): void
+    {
+        $this->parent = $parentDirectory;
+    }
+
+    public function addDirectory(Directory $directory)
+    {
+        $this->subdirectories[$directory->getName()] = $directory;
+    }
+
+    public function getParent(): Directory
+    {
+        return $this->parent;
+    }
 }
--- src/entities/FileTree.php
+++ src/entities/FileTree.php
@@ -2,16 +2,61 @@
 
 namespace Jdlcgarcia\Aoc2022\entities;
 
+use SplFileObject;
+
 class FileTree
 {
     private Directory $root;
+    private SplFileObject $listOfCommands;
+    private Directory $currentDirectory;
 
-    public function readCommand(string $command)
+    function __construct(SplFileObject $listOfCommands)
+    {
+        $this->listOfCommands = $listOfCommands;
+    }
+
+    public function run(): void
+    {
+        while (!$this->listOfCommands->eof()) {
+            if (trim($this->listOfCommands->current()) !== '') {
+                $this->readCommand(trim($this->listOfCommands->current()));
+            }
+
+            $this->listOfCommands->next();
+        }
+    }
+
+    public function readCommand(string $command): void
     {
         if (str_starts_with($command, '$')) {
             [$action, $directory] = sscanf($command, '$ %s %s');
-            echo $action . PHP_EOL;
+            match ($action) {
+                'cd' => $this->changeDirectory($directory),
+                'ls' => $this->listDirectory()
+            };
         }
+    }
+
+    private function changeDirectory(string $directory): void
+    {
+        $directoryToChange = new Directory($directory);
+        if ($directory === '/') {
+            $this->root = $directoryToChange;
+            $this->currentDirectory = $this->root;
+        } elseif ($directory === '..') {
+            $this->currentDirectory = $this->currentDirectory->getParent();
+        } else {
+            $directoryToChange->setParent($this->currentDirectory);
+            $this->currentDirectory->addDirectory($directoryToChange);
+            $this->currentDirectory = $this->currentDirectory->getDirectory($directory);
+        }
+
+         echo "I'm in directory ".$this->currentDirectory->getPath() . PHP_EOL;
+    }
+
+    private function listDirectory(): void
+    {
+        echo "Listing directory ".$this->currentDirectory->getPath() . PHP_EOL;
     }
 
 
