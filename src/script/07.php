8c101d2f4b487ae4999c479dbbded2195f80fa7b
Juan Garcia <jcruzgar@gmail.com>
2022-12-07 12:20:12 +0100

    feat(Day 07): moved file handling inside the tree

--- src/script/07.php
+++ src/script/07.php
@@ -8,11 +8,5 @@ use Jdlcgarcia\Aoc2022\entities\FileTree;
 $fileHandler = new FileHandler();
 $file = $fileHandler->loadFileContent('test.txt');
 
-$fileTree = new FileTree();
-while (!$file->eof()) {
-    if (trim($file->current()) !== '') {
-        $fileTree->readCommand(trim($file->current()));
-    }
-
-    $file->next();
-}
+$fileTree = new FileTree($file);
+$fileTree->run();
